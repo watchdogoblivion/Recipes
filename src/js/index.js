3948d3280927cd03ddfbad12e366dc481ec27415
watchdogoblivion <samueldorilas>
2018-08-25 19:04:50 -0400

    Built likes model, view, controller and implemented persistent data

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
+import * as likesView from './views/likesView';
 import {elements, renderLoader, clearLoader} from './views/base';
 
 /**
@@ -15,6 +17,7 @@ import {elements, renderLoader, clearLoader} from './views/base';
  */
 
 const state = {};
+window.state = state;
 
 const controlSearch = async () => {
     const query = searchView.getInput();
@@ -75,7 +78,10 @@ const controlRecipe = async () => {
             state.recipe.calcServings();
 
             clearLoader();
-            recipeView.renderRecipe(state.recipe);
+            recipeView.renderRecipe(
+                state.recipe,
+                state.likes.isLiked(id)
+            );
 
         }catch(error){
 
@@ -113,19 +119,40 @@ const controlList = () => {
     }
 }));
 
+const controlLike = () => {
+    if (!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
 
-    // elements.shopping.addEventListener('keydown', e => {
-    //     const id = e.target.closest('.shopping__item').dataset.itemid;
-    
-    //     if(e.keyCode === 13){
-    //         const val = parseFloat(e.target.value, 10);
-    //         state.list.updateCount(id, val);
-    //         console.log(state.list);
-    //     };
-    // })
+    if (!state.likes.isLiked(currentID)) {
+        const newLike = state.likes.addLike(
+            currentID, 
+            state.recipe.title,
+            state.recipe.author,
+            state.recipe.img
+        );
 
+        likesView.toggleLikedBtn(true);
+        likesView.renderLike(newLike);
 
+    } else {
+        state.likes.deleteLike(currentID);
+        likesView.toggleLikedBtn(false);
+        likesView.deleteLike(currentID);
 
+    }
+
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+}
+
+window.addEventListener('load', () => {
+    state.likes = new Likes();
+
+    state.likes.readStorage();
+
+    likesView.toggleLikeMenu(state.likes.getNumLikes());
+
+    state.likes.likes.forEach(like => likesView.renderLike(like));
+})
 elements.recipe.addEventListener('click', e => {
     if (e.target.matches('.btn-decrease, .btn-decrease *')) {
         if(state.recipe.servings > 1) {
@@ -137,8 +164,12 @@ elements.recipe.addEventListener('click', e => {
         recipeView.updateServingsIngredients(state.recipe);
     } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
         controlList();
+    } else if (e.target.matches('.recipe__love, .recipe__love *')) {
+        controlLike();
     }
 })
+
+//window.l = new List();
 
 // const r = new Recipe(47746);
 // r.getRecipe();
